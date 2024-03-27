1cd9f42e46214817de41d2fdee79473d670120c9
CaioHaendell <160561197+CaioHaendell@users.noreply.github.com>
2024-03-27 00:00:27 -0300

    Add files via upload

---
+++ Exemplos.js
@@ -0,0 +1,6 @@
+function indiceIMC(altura, peso) {
+    let altura = document.querySelector('input');
+    let peso = document.querySelector('input');
+    let calcImc = peso / altura;
+    return calcImc;
+    }
